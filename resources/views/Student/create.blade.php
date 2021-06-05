5c0a991885e61bde2b186016d6cbae767a619888
Selim Kurumahmut <1700002324@stu.iku.edu.tr>
2021-06-05 12:10:59 +0300

    Creating a view for new student

---
+++ resources/views/Student/create.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create a Student</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"></script>
+</head>
+<body>
+<div class="container">
+    <div class="row" style="margin-top: 45px;">
+        <div class="col-md-6">
+            <h4>Create</h4>
+            <form action="" method="post">
+                @csrf
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" name="name" placeholder="Enter Name">
+                </div>
+                <div class="form-group">
+                    <label for="surname">Surname</label>
+                    <input type="text" class="form-control" name="surname" placeholder="Enter Surname">
+                </div>
+                <div class="form-group">
+                    <label for="department">Department</label>
+                    <input type="text" class="form-control" name="department" placeholder="Enter Department">
+                </div>
+                <button type="submit" class="btn btn-block btn-primary" style="margin: 10px;">
+                    Save
+                </button>
+                <br>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
