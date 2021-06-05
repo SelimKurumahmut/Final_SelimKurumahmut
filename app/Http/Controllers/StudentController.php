b2a0dd207ac47c95257374e22328d64b89f0c933
Selim Kurumahmut <1700002324@stu.iku.edu.tr>
2021-06-05 12:14:25 +0300

    Create a controller function to save information

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    function main(){
+        return view('student.main');
+    }
+    function create(){
+        return view('student.create');
+    }
+    function save(Request $request){
+        $request->validate([
+            'name'=>'required|min:3',
+            'surname'=>'required',
+            'department'=>'required'
+        ]);
+
+        $student= new Student();
+        $student->name=$request->name;
+        $student->surname=$request->surname;
+        $student->department=$request->department;
+        $save=$student->save();
+
+        if($save){
+            return back()->with('success','Student created succesfully');
+        }
+        else{
+            return back()->with('fail','Fail. Try again.');
+        }
+    }
 }
